618fd99138aaca8e072b2bb8efc81716c202a639
titauhidd <tauhidultonmoy1234@gmail.com>
2025-07-29 03:25:20 +0600

    project setup

---
+++ src/Components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <h2>Hello Root</h2>
+    </div>
+  );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Root from "./Components/Root/Root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
